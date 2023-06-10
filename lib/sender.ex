5cd1a23a10bc1e637e36a4e2c07a7063d26ff621
Anthony Shull <shull.anthony@gmail.com>
2023-06-10 11:50:41 -0500

    using genserver

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,12 +1,19 @@
 defmodule Sender do
+  def send_email("poo@internet.com" = _email), do: :error
+
   def send_email(email) do
     Process.sleep(3000)
-    {:ok, "email sent to #{email}"}
+    response = "email sent to #{email}"
+    IO.puts(response)
+    {:ok, response}
   end
 
   def notify_all(emails) do
     Sender.TaskSupervisor
-    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1, max_concurrency: 2, ordered: false)
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1,
+      max_concurrency: 2,
+      ordered: false
+    )
     |> Enum.to_list()
   end
 end
